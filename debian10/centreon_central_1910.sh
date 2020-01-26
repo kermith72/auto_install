3562bb3be3bb53db21447f83d5eb860f496fac14
Eric Coquard <eric.coquard@gmail.com>
2020-01-26 22:20:27 +0100

    add web 19.10.5, engine 19.10.9

--- debian10/centreon_central_1910.sh
+++ debian10/centreon_central_1910.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Centreon 19.10 + engine install script for Debian Buster
-# v 1.42
-# 13/01/2020
+# v 1.43
+# 26/01/2020
 # Thanks to Remy, Justice81 and Pixelabs
 #
 export DEBIAN_FRONTEND=noninteractive
 # Variables
 ## Versions
-VERSION_BATCH="v 1.42"
+VERSION_BATCH="v 1.43"
 CLIB_VER=("19.10.0" "0")
 CONNECTOR_VER=("19.10.0" "0")
-ENGINE_VER=("19.10.8" "0")
+ENGINE_VER=("19.10.9" "0")
 PLUGIN_VER="2.2"
 PLUGIN_CENTREON_VER=("20191219" "0")
 BROKER_VER=("19.10.2" "0")
-CENTREON_VER=("19.10.4" "0")
+CENTREON_VER=("19.10.5" "0")
 # MariaDB Series
 MARIADB_VER='10.0'
 ## Sources URL
